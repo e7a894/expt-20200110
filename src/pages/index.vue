37b00674a8c18605d2955559a044cb022ea9702a
e7a894 <e7a894@outlook.jp>
2020-01-10 17:55:21 +0900

    feat: use vue-styled-components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>nuxt-template</h1>
+  <div>
+    <!-- flash -->
+    <styled-title>nuxt-template</styled-title>
+    <!-- not flash -->
+    <h1 class="test">
+      nuxt-template2
+    </h1>
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import styled from 'vue-styled-components';
+
+const StyledTitle = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+`;
+
+export default Vue.extend({
+  components: { StyledTitle }
+});
+</script>
+
+<style scoped>
+.test {
+  font-size: 2em;
+  text-align: right;
+  color: red;
+}
+</style>
